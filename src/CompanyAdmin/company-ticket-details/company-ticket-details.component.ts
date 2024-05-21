74c0eaad7454d45f9838cea76875ea2d5fdec327
phumu <phumu98@gmail.com>
2024-05-21 22:40:13 +0200

    Nazo

--- src/CompanyAdmin/company-ticket-details/company-ticket-details.component.ts
+++ src/CompanyAdmin/company-ticket-details/company-ticket-details.component.ts
@@ -1,5 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import jwtDecode from 'jwt-decode';
+import { StorageService } from 'src/app/utility/services/Storage/storage.service';
 
 @Component({
   selector: 'app-company-ticket-details',
@@ -8,7 +11,7 @@ import { Router } from '@angular/router';
 })
 export class CompanyTicketDetailsComponent {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,private http: HttpClient, private storage : StorageService) { }
 
   navigateToDestination() {
       this.router.navigate(['/employee-tickets']);
@@ -20,4 +23,31 @@ export class CompanyTicketDetailsComponent {
     const selectedFile = event.target.files[0];
     //console.log(selectedFile); Do something with the selected file
   }
+
+  dataArray:any[]=[]
+
+  
+
+  ngOnInit():void {
+
+    const token = this.storage.getUser()
+    const decodedToken:any=jwtDecode(token);
+    const companyId=decodedToken.companyId;
+
+
+    let url:string="http://localhost:8080/api/ticket/get-tickets/"+companyId;
+
+    this.http.get<any[]>(url).subscribe(data=>{
+      
+      this.dataArray=data;
+      console.log(data);
+    },
+  error=>
+  {
+    console.log(error);
+    
+  })
+
+  
+}
 }
