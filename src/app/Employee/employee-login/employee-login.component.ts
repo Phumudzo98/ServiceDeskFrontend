dc9ca1530eb5bc7e0d7be4daa94cce76da3cda11
Mkay\Mukovhe <mukovhemufamadi@gmail.com>
2024-05-23 09:21:05 +0200

    New Changes

--- src/app/Employee/employee-login/employee-login.component.ts
+++ src/app/Employee/employee-login/employee-login.component.ts
@@ -13,7 +13,7 @@ export class EmployeeLoginComponent {
   }
   loginForm: FormGroup = new FormGroup({
     email: new FormControl('',Validators.compose([Validators.required,Validators.email,Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')])),
-    password: new FormControl('', [Validators.required,Validators.minLength(16),Validators.maxLength(16),Validators.pattern(/^.{16}$/)]), 
+    password: new FormControl('',[Validators.required,Validators.maxLength(16),Validators.pattern(/^.{16}$/)]), 
    });
   get login (){return this.loginForm.controls;}
 
