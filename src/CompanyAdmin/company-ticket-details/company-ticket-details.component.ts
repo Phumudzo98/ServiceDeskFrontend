69798cbac0d91cf998041d81cc868e95789f4932
phumu <phumu98@gmail.com>
2024-05-26 23:48:03 +0200

    chatService

--- src/CompanyAdmin/company-ticket-details/company-ticket-details.component.ts
+++ src/CompanyAdmin/company-ticket-details/company-ticket-details.component.ts
@@ -1,10 +1,11 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import jwtDecode from 'jwt-decode';
-import * as SockJS from 'sockjs-client'
+import * as SockJS from 'sockjs-client';
 import { Client, IStompSocket, Message } from '@stomp/stompjs';
-import { Observable, map, of } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { StorageService } from 'src/app/utility/services/Storage/storage.service';
 import { RespondMessage } from 'src/app/utility/models/models';
 
@@ -13,102 +14,95 @@ import { RespondMessage } from 'src/app/utility/models/models';
   templateUrl: './company-ticket-details.component.html',
   styleUrls: ['./company-ticket-details.component.css']
 })
-export class CompanyTicketDetailsComponent {
+export class CompanyTicketDetailsComponent implements OnInit {
 
-  constructor(private router: Router, private http:HttpClient, private route: ActivatedRoute, private token: StorageService) { }
-
-  navigateToDestination() {
-      this.router.navigate(['/employee-tickets']);
-  }
-
-  //Button to select
-
-  onFileSelected(event: any) {
-    const selectedFile = event.target.files[0];
-    //console.log(selectedFile); Do something with the selected file
-  }
-
-  dataArray:any;
-  ticketId:any;
-  message:string=''
-  stompClient: Client = new Client;
+  dataArray: any;
+  ticketId: any;
+  message: string = '';
+  stompClient: Client = new Client();
   messageList?: Observable<Array<RespondMessage>>;
   messages: RespondMessage[] = [];
+  newMessage: string = '';
 
- ngOnInit(): void {
-   this.route.paramMap.subscribe(param=>
-     {
-       this.ticketId=param.get('ticketId');
-       
-       let url ="http://localhost:8080/api/ticket/get-ticket/"+this.ticketId;
-       
-       this.http.get<any>(url).subscribe(respone=>{
-        
-       this.dataArray=respone;
-         
-       },
-       error=>
-       {
-         console.log("Something went wrong");
-       })
+  constructor(
+    private router: Router,
+    private http: HttpClient,
+    private route: ActivatedRoute,
+    private token: StorageService
+  ) { }
 
-     }
-   )
-   this.connect();
- }
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(param => {
+      this.ticketId = param.get('ticketId');
 
- private connect(): void {
+      let url = "http://localhost:8080/api/ticket/get-ticket/" + this.ticketId;
 
-   this.stompClient.webSocketFactory = (): IStompSocket => {
-     return <IStompSocket>(<unknown>new SockJS('http://localhost:8081/chat'));
-   };
+      this.http.get<any>(url).subscribe(response => {
+        this.dataArray = response;
+      }, error => {
+        console.log("Something went wrong");
+      });
+    });
 
-   this.loadChat();
+    this.connect();
+  }
 
-   this.stompClient.onConnect = (frame) => {
-     
-     this.stompClient.subscribe("/topic/messages/" + this.ticketId, (message: Message) => {
-       const receivedMsg = JSON.parse(message.body) as RespondMessage;
-       this.messageList = this.messageList?.pipe(map((d: RespondMessage[]) => [...d, receivedMsg]))
-       this.messages.push(receivedMsg);
-     });
-   }
-   this.stompClient.activate();
- }
+  private connect(): void {
+    this.stompClient.webSocketFactory = (): IStompSocket => {
+      return new SockJS('http://localhost:8081/chat') as IStompSocket;
+    };
 
- public loadChat(): void {
-  this.messageList = this.http.get<Array<RespondMessage>>("http://localhost:8081/get-messages/" + this.ticketId);
+    this.loadChat();
 
-  this.messageList.subscribe(d => {
-    let mesg: Array<RespondMessage> = d;
-    
-    mesg.sort((a, b) => (a.timestamp > b.timestamp) ? 1 : -1);
-    this.messageList = of(mesg)
+    this.stompClient.onConnect = (frame) => {
+      this.stompClient.subscribe("/topic/messages/" + this.ticketId, (message: Message) => {
+        const receivedMsg = JSON.parse(message.body) as RespondMessage;
+        this.messageList = this.messageList?.pipe(map((d: RespondMessage[]) => [...d, receivedMsg]));
+        this.messages.push(receivedMsg);
+      });
+    }
 
-    console.log("Here");
-    
-  })
-}
+    this.stompClient.activate();
+  }
 
- sendMessage():void{
+  private loadChat(): void {
+    this.messageList = this.http.get<Array<RespondMessage>>("http://localhost:8081/get-messages/" + this.ticketId);
 
-  let getUserToken = this.token.getUser();
-  let getUserId=jwtDecode(getUserToken.accountId)
+    this.messageList.subscribe(d => {
+      let mesg: Array<RespondMessage> = d;
 
-   
-   let forwardMsg={
-    'sender':getUserId,
-    'content':'Hello',
-    'ticketId':this.ticketId
-   }
+      mesg.sort((a, b) => (a.timestamp > b.timestamp) ? 1 : -1);
+      this.messageList = of(mesg);
+      this.messages = mesg; // Add sorted messages to the messages array
 
-   this.stompClient?.publish({
-    destination: '/app/chat/' + this.ticketId,
-    body: JSON.stringify(forwardMsg)
-  });
-   console.log("haa");
-   
+      console.log("Here", mesg);
+    });
+  }
 
+  public sendMessage(): void {
+    let getUserToken = this.token.getUser();
+    let getUserId: any = jwtDecode(getUserToken);
+    let accountId = getUserId.accountId;
 
- }
+    let message = {
+      'sender': accountId,
+      'content': this.newMessage
+    };
+
+    this.stompClient.publish({
+      destination: '/app/chat/' + this.ticketId,
+      body: JSON.stringify(message)
+    });
+
+    this.newMessage = "";
+  }
+
+  navigateToDestination(): void {
+    this.router.navigate(['/employee-tickets']);
+  }
+
+  onFileSelected(event: any): void {
+    const selectedFile = event.target.files[0];
+    // Handle the selected file
+  }
 }
