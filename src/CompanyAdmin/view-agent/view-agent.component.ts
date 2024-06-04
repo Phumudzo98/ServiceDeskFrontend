d2f92ac09b2e7adce1c8af7f8197af6be89f6e6c
MFACTORY\nhlan <nhlanhlakhoza05@gmail.com>
2024-06-04 17:15:24 +0200

    ticket view

--- src/CompanyAdmin/view-agent/view-agent.component.ts
+++ src/CompanyAdmin/view-agent/view-agent.component.ts
@@ -11,7 +11,12 @@ import { Account } from 'src/app/utility/models/models';
 export class ViewAgentComponent {
   agent: any;
   private apiUrl = 'http://localhost:8080/api/users/get-agent'; // Replace with your actual API endpoint
-  TicketUser: any; // Initialize as an array to store ticket objects
+  TicketUser: any[] = []; // Initialize as an array to store ticket objects
+  searchQuery: string = '';
+  filteredTickets: any[] = [];
+  openTicketCount: number = 0;
+  closedTicketCount: number=0;
+  inProgressTicketCount:number=0;
   constructor(
     private route: ActivatedRoute,
     private http: HttpClient
@@ -22,6 +27,7 @@ export class ViewAgentComponent {
       const accountId = params.get('accountId');
       if (accountId) {
         this.getAgentById(accountId);
+        this.getUserTicket(accountId);
       } else {
         console.error('No accountId provided in route');
       }
@@ -41,13 +47,28 @@ export class ViewAgentComponent {
 
   getUserTicket(accountId: string): void {
     const url = `http://localhost:8080/api/ticket/get-user-tickets/${accountId}`;
-    this.http.get<any>(url).subscribe(
+    this.http.get<any[]>(url).subscribe(
       data => {
         this.TicketUser = data;
+        this.filteredTickets = data; // Initialize filtered tickets with the full list
+        this.countOpenTickets(); // Count the open tickets initially
+        this.countInProgressTickets();
+        this.countClosedTickets();
       },
       error => {
         console.error('Error fetching agent data', error);
       }
     );
+  }
+  countOpenTickets() {
+    this.openTicketCount = this.filteredTickets.filter(ticket => ticket.status === 'Open').length;
+  }
+
+  countClosedTickets() {
+    this.closedTicketCount = this.filteredTickets.filter(ticket => ticket.status === 'Closed').length;
+  }
+
+  countInProgressTickets() {
+    this.inProgressTicketCount = this.filteredTickets.filter(ticket => ticket.status === 'In progress').length;
   }
 }
